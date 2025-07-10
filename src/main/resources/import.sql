90d390800c79088d3c02ef1152db516c685f21b8
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-07-10 15:10:17 -0300

    Bcrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$Bhcw4tAiXmtMtrHOCNYzEeRoNCI4rgbgQ/OrP3RYb43Y71CZ7K0Hy');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$Bhcw4tAiXmtMtrHOCNYzEeRoNCI4rgbgQ/OrP3RYb43Y71CZ7K0Hy');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
